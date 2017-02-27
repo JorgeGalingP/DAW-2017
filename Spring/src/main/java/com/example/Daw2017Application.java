dbd3e7a46b86f992cad37acc122ab1bd88969f34
dani <danicamacho95@gmail.com>
2017-02-27 11:17:41 +0100

    Spring
    Añadioda la carpeta con Spring al repositorio

---
+++ Spring/src/main/java/com/example/Daw2017Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daw2017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Daw2017Application.class, args);
+	}
+}
