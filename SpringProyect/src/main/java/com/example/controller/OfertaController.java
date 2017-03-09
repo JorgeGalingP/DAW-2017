d29186e69dddb70335dfb42844521cc69749687e
iliarz <iliarz@gmail.com>
2017-03-09 11:26:12 +0100

    crear ofertas normales desde el formulario

--- SpringProyect/src/main/java/com/example/controller/OfertaController.java
+++ SpringProyect/src/main/java/com/example/controller/OfertaController.java
@@ -3,15 +3,20 @@ package com.example.controller;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.example.entity.Oferta;
 
 import com.example.repository.OfertaRepository;
+
+
 
 @Controller
 public class OfertaController {
@@ -29,14 +34,24 @@ public class OfertaController {
 	
 	}
 	
-	@RequestMapping(value="/{id}",method = RequestMethod.GET)
+	/*@RequestMapping(value="/{id}",method = RequestMethod.GET)
 	public String oferta (Model model ,@PathVariable Integer id){
 	
 	        Oferta oferta=  ofertaRepository.findOne(id);
 	        model.addAttribute("oferta",oferta);
 	        
 	        return "oferta";
-	}
+	}*/
+	
+	
+	//metodo para crear una oferta desde el form
+	@RequestMapping("/nuevaOferta")
+	public String nuevaOferta(Model model, Oferta oferta) {
+
+		ofertaRepository.save(oferta);
+		return "ofertas";
+		
+		}
 	
 	
 	
