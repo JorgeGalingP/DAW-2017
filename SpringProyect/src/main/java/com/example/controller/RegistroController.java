95a85ba85dc406ceeffe33235957d1f7e7fe19d7
iliarz <iliarz@gmail.com>
2017-03-20 20:22:22 +0100

    registro + webcontroller

--- SpringProyect/src/main/java/com/example/controller/RegistroController.java
+++ SpringProyect/src/main/java/com/example/controller/RegistroController.java
@@ -1,32 +1,39 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.entity.User;
+import com.example.repository.ResourceRepository;
+import com.example.repository.UserRepository;
+
 @Controller
 public class RegistroController {
 	
-	@RequestMapping("/inicio")
-	public String miPerfil(Model model, @RequestParam String  name,@RequestParam String surname,@RequestParam String  email, @RequestParam String  telephone,@RequestParam String pais,@RequestParam String codigoPostal,@RequestParam String  descripcion,@RequestParam String inputPassword){
+
+	@Autowired 
+	private UserRepository userRepository;
+	
+	@RequestMapping("/registrarse")
+	public String Registro( Model model, User user){
 		
 		
-		model.addAttribute("name",name);
-		model.addAttribute("surname",surname);
-		model.addAttribute("email",email);
-		model.addAttribute("telephone",telephone);
-		model.addAttribute("pais",pais);
-		model.addAttribute("codigoPostal",codigoPostal);
-		model.addAttribute("descripcion", descripcion);
-		model.addAttribute("inputPassword",inputPassword);
+		userRepository.save(user);
 		
+		
+		model.addAttribute("usuario",user);
+	
 
 
 		return "miperfil";	
 		
 	}
-	@RequestMapping("/Modificar")
+	
+	
+	/*@RequestMapping("/Modificar")
 	public String miPerfil2 (Model model ,@RequestParam String name,@RequestParam String surname,@RequestParam String email,@RequestParam String telephone,@RequestParam String pais,@RequestParam String descripcion,@RequestParam String inputPassword,@RequestParam String codigoPostal){
 		
 		model.addAttribute("name",name);
@@ -40,6 +47,6 @@ public class RegistroController {
 		
 		return"miperfil";
 		
-	}
+	}*/
 
 }
