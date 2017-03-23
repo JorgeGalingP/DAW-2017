0e7169d8c0d4d1b3ffb99cb7513c0c8bbabd62a5
Jorge Galindo Peña <jorgape225@gmail.com>
2017-03-23 20:21:16 +0100

    Revert "Paginacion"
    This reverts commit c252486971711c668b66bc655ea8b1fe3af06a3d.

--- SpringProyect/src/main/java/com/example/controller/ViniloController.java
+++ SpringProyect/src/main/java/com/example/controller/ViniloController.java
@@ -144,7 +144,15 @@ public class ViniloController {
 					
 					}
 				
-			
+				@RequestMapping("/moreVinilos")
+				public String vinilos(Model model,@RequestParam int page){
+					
+					Page<Resource> resources = repository.findAll(new PageRequest(page,12));
+					model.addAttribute("items","resources");
+					
+					return"listItemsPage";
+
+					}
 				
 		/*@RequestMapping("/articulo/addComment")
 		public void addComment(Comment comment, @PathVariable int id) {
