521969fda529649ad122858aebf1e4c2d1d2028c
dani <danicamacho95@gmail.com>
2017-04-18 19:58:36 +0200

    Añadida estructura y componente del footer y html
    añadida nuevaas componentes y html

---
+++ frontend/src/app/util.ts
@@ -0,0 +1,11 @@
+export const BASE_URL ='https//localhost:8443/api';
+
+export const LOGIN_URL  = BASE_URL +"login";
+export const RESOURCE_URL = BASE_URL +"resource"
+export const OFERTA_URL = BASE_URL +"oferta"
+export const OFERTADESCUENTO_URL = BASE_URL+"ofertaDescuento"
+export const USER_URL = BASE_URL +"users";
+
+export const IMG_URL  ="./assets/img/";
+
+export const Status_NO_CONTENT = 204;
