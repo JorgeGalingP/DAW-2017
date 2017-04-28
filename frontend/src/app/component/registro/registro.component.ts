e90a6b50dd6efbcbda1c6b7ad70cd54e5286fe93
iliarz <iliarz@gmail.com>
2017-04-28 14:04:23 +0200

    logincomponent y registrocomponent creados

---
+++ frontend/src/app/component/registro/registro.component.ts
@@ -0,0 +1,8 @@
+import{Component,EventEmitter,Output} from '@angular/core';
+
+@Component({
+    selector:'app-registro',
+    templateUrl:'registro.component.html'
+})
+
+export class RegistroComponent{}
