5b7187aa6ade702f72242dc5af6c45eceb039a4e
dani <danicamacho95@gmail.com>
2017-05-23 20:12:36 +0200

    new change

--- frontend/src/app/component/index/index.component.ts
+++ frontend/src/app/component/index/index.component.ts
@@ -2,27 +2,29 @@ import{Component} from '@angular/core';
 import{OnInit} from'@angular/core';
 import{Router,ActivatedRoute} from'@angular/router';
 import{Resource}  from'app/models/resource.model';
-import{ResourceService} from 'app/service/resource.service';
+import{ResourceService,Resources} from 'app/service/resource.service';
 import{User}from'../../models/user.model'
+
 
 @Component({
     selector:'app-index',
     templateUrl:'index.component.html'
 })
 
-export class IndexComponent implements OnInit{
+export class IndexComponent /*implements OnInit*/{
 
-  resources:Resource[];
+  resources:Resources[];
 
   constructor(private router:Router, private service:ResourceService){
+    this.resources = service.getResources();
   }
  
-   ngOnInit(){
+  /* ngOnInit(){
      this.service.getResources().subscribe(
        resources => this.resources = resources,
        error => console.log(error)
      )
-   }
+   }*/
 
   
 
--- frontend/src/app/service/resource.service.ts
+++ frontend/src/app/service/resource.service.ts
@@ -3,13 +3,33 @@ import{Http,Response}from'@angular/http'
 import{Observable} from 'rxjs/Observable'
 import'rxjs/Rx';
 
-import {Resource} from"app/models/resource.model";
+//import {Resource} from"app/models/resource.model";
  const BASE_URL ="https://localhost:8443/api/resources/"
 
+export class Resources{
+    constructor(public id:number,public title:string,public subtitule:string,public interprete:string,public fecha:string,public estado:string,public precio: number, public código:number, public img:string ){}
+
+}
 @Injectable()
 export class ResourceService{
 
-    constructor (private http:Http){
+    Resources=[
+        new Resources(8,"hola","fjfjf","Ffff","fffff","dddd",88999,999,""),
+        new Resources(9,"dddfj","fjfjf","Ffff","fffff","dddd",88999,999,""),
+        new Resources(10,"dhfhfd","fjfjf","Ffff","fffff","dddd",88999,999,""),
+        new Resources(11,"dddd","fjfjf","Ffff","fffff","dddd",88999,999,""),
+        new Resources(12,"dddd","fjfjf","Ffff","fffff","dddd",88999,999,""),
+        new Resources(13,"dddd","fjfjf","Ffff","fffff","dddd",88999,999,""),
+        new Resources(14,"dddd","fjfjf","Ffff","fffff","dddd",88999,999,"")
+    
+
+    ];
+
+    getResources(){
+        return this.Resources;
+    }
+
+  /*  constructor (private http:Http){
 
     }
 
@@ -22,7 +42,8 @@ export class ResourceService{
     private handleError(error: any) {
 		console.error(error);
 		return Observable.throw("Server error (" + error.status + "): " + error.text())
-	}
+	}*/
+
     
 
 
