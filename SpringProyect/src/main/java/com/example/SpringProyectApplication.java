441208646fdf1ba2e22e43fa1409fc657f5d8afd
dani <danicamacho95@gmail.com>
2017-05-30 11:56:46 +0200

    Update SpringProyectApplication.java

--- SpringProyect/src/main/java/com/example/SpringProyectApplication.java
+++ SpringProyect/src/main/java/com/example/SpringProyectApplication.java
@@ -9,5 +9,20 @@ public class SpringProyectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringProyectApplication.class, args);
+		
+	}
+	@Bean
+
+	public WebMvcConfigurerAdapter corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")
+						.allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin",
+								"Access-Control-Request-Method", "Access-Control-Request-Headers")
+						.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+						.allowCredentials(true).maxAge(3600);
+			}
+		};
 	}
 }
